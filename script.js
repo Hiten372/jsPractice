41fba3ef362805019754d32637797328a1aa2c9a
hiten <hitenprasad2017jsr@gmail.com>
2026-07-20 20:50:52 +0530

    Q1

---
+++ script.js
@@ -0,0 +1,44 @@
+// practice work
+
+console.log(
+  `Helloow Developer  \nDATE:${Date().toString().split(" ").slice(0, 5).join("-")}`,
+);
+
+//date 20-july-2026 --------------------------------------------->
+// Q1 filter users based on salary and covert name and department to uppercase.
+
+/*
+const emp = [
+  {
+    name: "hiten prasad",
+    salary: 45000,
+    department: "developement",
+  },
+  {
+    name: "yash prasad",
+    salary: 60000,
+    department: "design",
+  },
+  {
+    name: "nihal prasad",
+    salary: 30000,
+    department: "developement",
+  },
+  {
+    name: "neha KUMARI",
+    salary: 70000,
+    department: "management",
+  },
+];
+
+let filterUsers = emp
+  .filter((info) => info.salary > 40000)
+  .map((info) => ({
+    name: info.name.toUpperCase(),
+    salary: info.salary,
+    department: info.department.toUpperCase(),
+  }));
+console.log(filterUsers);
+*/
+
+//
